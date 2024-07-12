1879e3d6e2ac38f6c10334aeadf9fa3428cf2b8a
tannukumari14 <tannukumari21@navgurukul.org>
2024-07-12 21:04:22 +0530

    no commits

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./globals.css";   
+import Header from "./header";
 
 const Page = () => {
   return (
     <div className="page-container">
       <div className="background-image">
         <div className="content">
+          <Header/>
           <h1>Welcome to My Weather App</h1>
-          <p>Find current weather conditions:</p>
+          <p id="paragraph">Find current weather conditions:</p>
           <form className="search-form">
             <input type="text" placeholder="Enter location..." />
             <button type="submit">Search</button>
